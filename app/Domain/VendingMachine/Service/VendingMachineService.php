c529c628c599f7755ab868813d618255e0ed4c1a
Luis Garcés <info@luisarturogarcesdiaz.com>
2025-10-10 14:11:19 +0200

    feat: add VendingMachineService

---
+++ app/Domain/VendingMachine/Service/VendingMachineService.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Domain\VendingMachine\Service;
+
+class VendingMachineService
+{
+    private int $balance = 0;
+    private array $products = [];
+    private array $coins = [];
+    private array $coins_machine = [];
+    private array $coins_introduced = [];
+
+    public function __construct(int $balance, array $products, array $coins_machine, array $coins_introduced)
+    {
+        $this->balance = $balance;
+
+        foreach ($products as $product) {
+            $this->products[$product->getSku()] = $product;
+        }
+
+        foreach ($coins_machine as $coin) {
+            $this->coins[$coin->getValue()] = $coin;
+            $this->coins_machine[$coin->getValue()] = $coin->getQuantity();
+        }
+
+        $this->coins_introduced = $coins_introduced;
+    }
+
+    public function getBalance(): int
+    {
+        return $this->balance;
+    }
+
+    public function getProducts(): array
+    {
+        return $this->products;
+    }
+
+    public function getCoinsMachine(): array
+    {
+        return $this->coins_machine;
+    }
+
+    public function getCoinsMachineData(): array
+    {
+        $coins = [];
+        foreach ($this->coins_machine as $coin) {
+            $coins[$coin->getValue()] = $coin->getQuantity();
+        }
+        return $coins;
+    }
+
+    public function getCoinsIntroduced(): array
+    {
+        return $this->coins_introduced;
+    }
+
+    public function getCoinsIntroducedData(): array
+    {
+        $coins = [];
+        foreach ($this->coins_introduced as $coin) {
+            $coins[$coin->getValue()] = $coin->getQuantity();
+        }
+        return $coins;
+    }
+
+    public function setBalance(int $balance): void
+    {
+        $this->balance = $balance;
+    }
+
+    public function setProducts(array $products): void
+    {
+        foreach ($products as $product) {
+            $this->products[$product->getSku()] = $product;
+        }
+    }
+
+    public function setCoinsMachine(array $coins_machine): void
+    {
+        $this->coins_machine = [];
+
+        foreach ($coins_machine as $coin) {
+            $this->coins_machine[$coin->getValue()] = $coin;
+        }
+    }
+
+    public function setCoinsIntroduced(array $coins_introduced): void
+    {
+        $this->coins_introduced = [];
+
+        foreach ($coins_introduced as $coin) {
+            $this->coins_introduced[$coin->getValue()] = $coin;
+        }
+    }
+}
--- app/Infrastructure/Http/Controllers/VendingMachineController.php
+++ app/Infrastructure/Http/Controllers/VendingMachineController.php
@@ -2,12 +2,23 @@
 
 namespace App\Infrastructure\Http\Controllers;
 
+use App\Domain\VendingMachine\Service\VendingMachineService;
 use App\Infrastructure\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class VendingMachineController extends Controller
 {
-    public function __construct() {}
+    private VendingMachineService $service;
+
+    public function __construct()
+    {
+        $this->service = new VendingMachineService(
+            balance: 0,
+            products: [],
+            coins_machine: [],
+            coins_introduced: []
+        );
+    }
 
     public function index() {}
 
