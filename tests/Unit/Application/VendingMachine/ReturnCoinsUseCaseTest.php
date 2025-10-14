d25b961880e3f433ac1c31af9c8b3742c97bcd5c
Luis Garcés <info@luisarturogarcesdiaz.com>
2025-10-14 22:50:41 +0200

    fix: Unify ReturnCoinsUseCaseTest test_ok

--- tests/Unit/Application/VendingMachine/ReturnCoinsUseCaseTest.php
+++ tests/Unit/Application/VendingMachine/ReturnCoinsUseCaseTest.php
@@ -33,13 +33,7 @@ class ReturnCoinsUseCaseTest extends TestCase
             $this->assertArrayHasKey($coin, $response->coins_machine);
             $this->assertEquals($default_quantity, $response->coins_machine[$coin]);
         }
-    }
 
-    public function test_ok_with_introduced(): void
-    {
-        $service = new FakeVendingMachineService();
-
-        $default_quantity = 10;
         $introduced_quantity = 2;
         $service->setCoinsMachine([
             new Coin(FakeVendingMachineService::$DEFAULT_COINS_MACHINE_DATA[0]['value'], $default_quantity),
@@ -52,7 +46,6 @@ class ReturnCoinsUseCaseTest extends TestCase
         ]);
         $service->setBalance(50);
 
-        $useCase = new ReturnCoinsUseCase($service);
         $response = $useCase->execute();
 
         $this->assertEquals(0, $service->getBalance());
